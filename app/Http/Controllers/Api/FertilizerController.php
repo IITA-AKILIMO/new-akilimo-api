ac8e715578bacbefdaec6f74fec054dad224d22f
Masgeek <barsamms@gmail.com>
2025-04-23 14:48:58 +0300

    ✨ **feat(api): add `byCountry` method to FertilizerController**
    Introduced `byCountry` method to filter fertilizers by country and availability, with customizable pagination and sorting. Removed unused methods (`store`, `show`, `update`, `destroy`) for a leaner controller.
    
    **Breaking Changes:** Renamed and repurposed method `store` to `byCountry`. Existing API calls to `store` will no longer work.

--- app/Http/Controllers/Api/FertilizerController.php
+++ app/Http/Controllers/Api/FertilizerController.php
@@ -20,9 +20,8 @@ class FertilizerController extends Controller
         $sort = $request->input('sort', 'asc'); // Default sort order is ascending
 
         $currencies = Fertilizer::query()
-            ->when($country != null, function ($query) use ($country) {
-                return $query->where('country', strtoupper($country));
-            })
+            ->where('country', strtoupper(trim($country)))
+            ->where('available', true)
             ->orderBy($orderBy, $sort)
             ->paginate($perPage);
 
@@ -30,34 +29,23 @@ class FertilizerController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @param string $countryCode
+     * @param Request $request
+     * @return FertilizerResourceCollection
      */
-    public function store(Request $request)
+    public function byCountry(string $countryCode, Request $request)
     {
-        //
+        $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
+        $orderBy = $request->input('order_by', 'sort_order'); // Default order by invoice_date
+        $sort = $request->input('sort', 'asc'); // Default sort order is ascending
+
+        $currencies = Fertilizer::query()
+            ->where('country', strtoupper(trim($countryCode)))
+            ->where('available', true)
+            ->orderBy($orderBy, $sort)
+            ->paginate($perPage);
+
+        return FertilizerResourceCollection::make($currencies);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Fertilizer $fertilizer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Fertilizer $fertilizer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Fertilizer $fertilizer)
-    {
-        //
-    }
 }
