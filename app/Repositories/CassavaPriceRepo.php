32f3db6b5fb5996b69355e122c768ccf2d5ba156
Masgeek <barsamms@gmail.com>
2025-04-24 09:11:15 +0300

    ♻️ refactor(CassavaPriceRepo): rename method for clarity and minor formatting adjustments
    Renamed `findMinMaxPriceEntityByCountryCode` to `findPriceBandsByCountryCode` for better readability and consistency. Adjusted parameter alignment and removed redundant blank lines for improved code formatting.

--- app/Repositories/CassavaPriceRepo.php
+++ app/Repositories/CassavaPriceRepo.php
@@ -5,24 +5,21 @@ namespace App\Repositories;
 use App\Data\MinMaxPriceDto;
 use App\Models\CassavaPrice;
 
-
 class CassavaPriceRepo
 {
-
     /**
      * Retrieves the minimum and maximum price entity for a given country code.
      *
-     * @param string $countryCode The country code used to filter the prices.
+     * @param  string  $countryCode  The country code used to filter the prices.
      * @return MinMaxPriceDto Contains the minimum and maximum local prices for the specified country.
      */
-    public function findMinMaxPriceEntityByCountryCode(string $countryCode): MinMaxPriceDto
+    public function findPriceBandsByCountryCode(string $countryCode): MinMaxPriceDto
     {
         $minPrice = CassavaPrice::whereCountry($countryCode)
             ->where('min_local_price', '>', 0)
             ->min('min_local_price');
 
         $maxPrice = CassavaPrice::whereCountry($countryCode)->max('max_local_price');
-
 
         return new MinMaxPriceDto($minPrice, $maxPrice);
     }
