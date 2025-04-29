1354c0ef57a227a60130e7b1ac91acc846bc748b
Masgeek <barsamms@gmail.com>
2025-04-29 10:20:08 +0300

    ♻️ refactor(resources): rename variable for clarity in InvestmentAmountResource
    - Renamed variable `$amount` to `$model` for consistency and clarity.
    - Updated all references to reflect the new variable name.

--- app/Http/Resources/InvestmentAmountResource.php
+++ app/Http/Resources/InvestmentAmountResource.php
@@ -15,23 +15,23 @@ class InvestmentAmountResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        /** @var InvestmentAmount $amount */
-        $amount = $this->resource;
+        /** @var InvestmentAmount $model */
+        $model = $this->resource;
 
-        $tag = $amount->id;
-        if ($amount->investment_amount === -1.0) {
+        $tag = $model->id;
+        if ($model->investment_amount === -1.0) {
             $tag = 'exact';
         }
 
         return [
-            'id' => $amount->id,
-            'investment_amount' => $amount->investment_amount,
-            'country_code' => $amount->country,
-            'exact_amount' => $amount->investment_amount === 0.0,
+            'id' => $model->id,
+            'investment_amount' => $model->investment_amount,
+            'country_code' => $model->country,
+            'exact_amount' => $model->investment_amount === 0.0,
             'item_tag' => $tag,
-            'active' => $amount->price_active,
-            'area_unit' => $amount->area_unit,
-            'sort_order' => $amount->sort_order,
+            'active' => $model->price_active,
+            'area_unit' => $model->area_unit,
+            'sort_order' => $model->sort_order,
         ];
     }
 }
