d8daeff65f1d144a4b89b41ba645e4e7bd6503e2
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-21 15:20:16 +0300

    ✨ feat(PlumberComputeData): add input mapping for fertilizer attributes
    - Introduced `#[MapInputName]` attributes to map input keys for various fertilizer-related properties.
    - Ensured consistency between input (`MapInputName`) and output (`MapOutputName`) naming for all fertilizer fields.
    
    Provides better flexibility and alignment when processing input data.

--- app/Data/FertilizerData.php
+++ app/Data/FertilizerData.php
@@ -2,20 +2,9 @@
 
 namespace App\Data;
 
-use Illuminate\Contracts\Pagination\CursorPaginator as CursorPaginatorContract;
-use Illuminate\Contracts\Pagination\Paginator as PaginatorContract;
-use Illuminate\Pagination\AbstractCursorPaginator;
-use Illuminate\Pagination\AbstractPaginator;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Enumerable;
-use Illuminate\Support\LazyCollection;
 use Spatie\LaravelData\Attributes\MapInputName;
-use Spatie\LaravelData\Attributes\MapOutputName;
-use Spatie\LaravelData\CursorPaginatedDataCollection;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\DataCollection;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
-use Spatie\LaravelData\PaginatedDataCollection;
 
 #[MapInputName(SnakeCaseMapper::class)]
 class FertilizerData extends Data
@@ -23,16 +12,20 @@ class FertilizerData extends Data
 
     public string $name;
 
+    #[MapInputName("fertilizer_label")]
+    public string $label;
+
     #[MapInputName("fertilizer_type")]
     public string $type;
 
+    #[MapInputName("key")]
     public string $key;
 
     public float $weight;
 
     #[MapInputName("price")]
-    public float $pricePerBag;
+    public float $pricePerBag = 0.0;
 
     #[MapInputName("selected")]
-    public bool $available;
+    public bool $selected = false;
 }
