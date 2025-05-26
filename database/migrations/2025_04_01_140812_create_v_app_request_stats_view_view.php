1263e1a3f261d35d53b982dd8dfbde53e7fdb1bc
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-05-26 10:05:57 +0300

    ✨ feat(database): improve app request stats view creation
    Refactored the migration script for the `v_app_request_stats_view` database view to enhance readability and maintainability. Introduced a `private` class variable for the view name and utilized `CREATE OR REPLACE VIEW` to streamline updates.
    
    BREAKING CHANGE: The migration alters the logic for creating the `v_app_request_stats_view`, ensuring compatibility with the updated query format. Existing setups relying on the previous definition may need adjustments.

--- database/migrations/2025_04_01_140812_create_v_app_request_stats_view_view.php
+++ database/migrations/2025_04_01_140812_create_v_app_request_stats_view_view.php
@@ -3,14 +3,59 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class extends Migration {
+    private string $viewName = 'v_app_request_stats_view';
+
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        DB::statement("CREATE VIEW `v_app_request_stats_view` AS select `akilimo_new`.`app_report`.`id` AS `id`,`akilimo_new`.`app_report`.`created_at` AS `request_date`,`akilimo_new`.`app_report`.`device_token` AS `device_token`,`akilimo_new`.`app_report`.`country_code` AS `country_code`,`akilimo_new`.`app_report`.`lat` AS `lat`,`akilimo_new`.`app_report`.`lon` AS `lon`,ucase(`akilimo_new`.`app_report`.`full_names`) AS `full_names`,`akilimo_new`.`app_report`.`gender` AS `gender_name`,`akilimo_new`.`app_report`.`excluded` AS `excluded`,case when `akilimo_new`.`app_report`.`gender` = 'Male' then 'M' when `akilimo_new`.`app_report`.`gender` = 'Mwanaume' then 'M' when `akilimo_new`.`app_report`.`gender` = 'Female' then 'F' when `akilimo_new`.`app_report`.`gender` = 'Mwanamke' then 'F' else 'NA' end AS `gender`,`akilimo_new`.`app_report`.`phone_number` AS `phone_number`,ifnull(`akilimo_new`.`user_feedback`.`user_type`,'OTHER') AS `user_type`,case when `akilimo_new`.`app_report`.`fr` = 1 then 'FR' when `akilimo_new`.`app_report`.`pp` = 1 then 'PP' when `akilimo_new`.`app_report`.`ic` = 1 then 'IC' when `akilimo_new`.`app_report`.`spp` = 1 or `akilimo_new`.`app_report`.`sph` = 1 then 'SPHS' else 'NA' end AS `use_case`,`akilimo_new`.`app_report`.`created_at` AS `created_at`,`akilimo_new`.`app_report`.`updated_at` AS `updated_at` from (`akilimo_new`.`app_report` left join `akilimo_new`.`user_feedback` on(`akilimo_new`.`app_report`.`device_token` = `akilimo_new`.`user_feedback`.`device_token`))");
+        $sql = <<<SQL
+CREATE OR REPLACE VIEW $this->viewName AS SELECT
+  app_report.id AS id,
+  app_report.created_at AS request_date,
+  app_report.device_token AS device_token,
+  app_report.country_code AS country_code,
+  app_report.lat AS lat,
+  app_report.lon AS lon,
+  ucase(`app_report`.`full_names`) AS full_names,
+  app_report.gender AS gender_name,
+  app_report.excluded AS excluded,
+  CASE
+    WHEN `app_report`.`gender` = 'Male' THEN
+      'M'
+    WHEN `app_report`.`gender` = 'Mwanaume' THEN
+      'M'
+    WHEN `app_report`.`gender` = 'Female' THEN
+      'F'
+    WHEN `app_report`.`gender` = 'Mwanamke' THEN
+      'F'
+    ELSE
+      'NA'
+  END AS gender,
+  app_report.phone_number AS phone_number,
+  ifnull(`user_feedback`.`user_type`, 'OTHER') AS user_type,
+  CASE
+    WHEN `app_report`.`fr` = 1 THEN
+      'FR'
+    WHEN `app_report`.`pp` = 1 THEN
+      'PP'
+    WHEN `app_report`.`ic` = 1 THEN
+      'IC'
+    WHEN `app_report`.`spp` = 1
+      OR `app_report`.`sph` = 1 THEN
+      'SPHS'
+    ELSE
+      'NA'
+  END AS use_case,
+  app_report.created_at AS created_at,
+  app_report.updated_at AS updated_at
+FROM
+  (app_report);
+SQL;
+
+        DB::statement(query: $sql);
     }
 
     /**
@@ -18,6 +63,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::statement("DROP VIEW IF EXISTS `v_app_request_stats_view`");
+        DB::statement("DROP VIEW IF EXISTS {$this->viewName}");
     }
 };
