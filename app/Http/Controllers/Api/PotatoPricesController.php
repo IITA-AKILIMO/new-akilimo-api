24d8b73aa6f8d6d0c4d32206a6363d621d35f3b7
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-10-24 23:41:54 +0300

    ✨ feat(controllers): add index and byCountry methods to PotatoPricesController
    Added `index` and `byCountry` methods to `PotatoPricesController`. These methods allow fetching active potato price records with support for customizable pagination (`per_page`) and sorting (`order_by`, `sort`). The `byCountry` method further enables filtering records by a specific country. This enhancement improves the API's flexibility and usability for clients retrieving potato price data.

--- app/Http/Controllers/Api/PotatoPricesController.php
+++ app/Http/Controllers/Api/PotatoPricesController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Collections\PotatoPriceResourceCollection;
+use App\Models\PotatoPrice;
 use Illuminate\Http\Request;
 
 class PotatoPricesController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
+        $orderBy = $request->input('order_by', 'sort_order'); // Default order by invoice_date
+        $sort = $request->input('sort', 'asc'); // Default sort order is ascending
+
+        $cassavaPrices = PotatoPrice::query()
+            ->where('price_active', true)
+            ->orderBy($orderBy, $sort)
+            ->paginate($perPage);
+
+        return PotatoPriceResourceCollection::make($cassavaPrices);
+    }
+
+    public function byCountry(string $countryCode, Request $request)
+    {
+        $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
+        $orderBy = $request->input('order_by', 'sort_order'); // Default order by invoice_date
+        $sort = $request->input('sort', 'asc'); // Default sort order is ascending
+
+        $cassavaPrices = PotatoPrice::query()
+            ->where('country', strtoupper(trim($countryCode)))
+            ->where('price_active', true)
+            ->orderBy($orderBy, $sort)
+            ->paginate($perPage);
+
+        return PotatoPriceResourceCollection::make($cassavaPrices);
+    }
 }
