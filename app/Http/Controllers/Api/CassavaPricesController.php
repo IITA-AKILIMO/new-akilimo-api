c66b513f011a7e94811fce5a995a5a0a4339ce86
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-10-24 23:41:40 +0300

    ✨ feat(controllers): add index method to CassavaPricesController
    Introduced an `index` method in `CassavaPricesController` to fetch active cassava price records with pagination support. The method includes customizable sorting and ordering options (`order_by`, `sort`), improving the API flexibility for data retrieval.

--- app/Http/Controllers/Api/CassavaPricesController.php
+++ app/Http/Controllers/Api/CassavaPricesController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Collections\CassavaPriceResourceCollection;
-use App\Http\Resources\Collections\StarchFactoryResourceCollection;
 use App\Models\CassavaPrice;
-use App\Models\StarchFactory;
 use Illuminate\Http\Request;
 
 class CassavaPricesController extends Controller
 {
+    public function index(Request $request)
+    {
+        $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
+        $orderBy = $request->input('order_by', 'sort_order'); // Default order by invoice_date
+        $sort = $request->input('sort', 'asc'); // Default sort order is ascending
+
+        $cassavaPrices = CassavaPrice::query()
+            ->where('price_active', true)
+            ->orderBy($orderBy, $sort)
+            ->paginate($perPage);
+
+        return CassavaPriceResourceCollection::make($cassavaPrices);
+    }
+
     public function byCountry(string $countryCode, Request $request)
     {
         $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
