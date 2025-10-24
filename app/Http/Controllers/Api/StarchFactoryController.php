7f2731b3272c9ee1f3a2772a0aa7d152fe3acc48
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-10-24 23:16:22 +0300

    ✨ feat(controllers): add index method to StarchFactoryController
    Introduced an `index` method in `StarchFactoryController` to fetch all active starch factories with pagination support. The method includes customizable sorting and ordering options (`order_by`, `sort`), enhancing API functionality for consuming clients.

--- app/Http/Controllers/Api/StarchFactoryController.php
+++ app/Http/Controllers/Api/StarchFactoryController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Collections\StarchFactoryResourceCollection;
-use App\Http\Resources\StarchFactoryResource;
 use App\Models\StarchFactory;
 use Illuminate\Http\Request;
 
 class StarchFactoryController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
+        $orderBy = $request->input('order_by', 'sort_order'); // Default order by invoice_date
+        $sort = $request->input('sort', 'asc'); // Default sort order is ascending
+
+        $starchFactory = StarchFactory::query()
+            ->where('factory_active', true)
+            ->orderBy($orderBy, $sort)
+            ->paginate($perPage);
+
+        return StarchFactoryResourceCollection::make($starchFactory);
+    }
 
     public function byCountry(string $countryCode, Request $request)
     {
