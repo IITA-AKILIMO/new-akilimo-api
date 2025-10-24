b2b62e4e043467d403cca250904d51be116510af
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-10-24 23:41:47 +0300

    ✨ feat(controllers): add index method to MaizePricesController
    Introduced an `index` method in `MaizePricesController` to fetch active maize price records with pagination support. The method includes customizable sorting (`order_by`, `sort`) and customizable pagination (`per_page`), improving API flexibility for data retrieval.

--- app/Http/Controllers/Api/MaizePricesController.php
+++ app/Http/Controllers/Api/MaizePricesController.php
@@ -9,6 +9,20 @@ use Illuminate\Http\Request;
 
 class MaizePricesController extends Controller
 {
+    public function index(Request $request)
+    {
+        $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
+        $orderBy = $request->input('order_by', 'sort_order'); // Default order by invoice_date
+        $sort = $request->input('sort', 'asc'); // Default sort order is ascending
+
+        $maizePrices = MaizePrice::query()
+            ->where('price_active', true)
+            ->orderBy($orderBy, $sort)
+            ->paginate($perPage);
+
+        return MaizePriceResourceCollection::make($maizePrices);
+    }
+
     public function byCountry(string $countryCode, Request $request)
     {
         $perPage = $request->input('per_page', 50); // Number of records per page, default is 50
