36dffebd1f7e11496994c11fb673a97462a359a2
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-11-01 01:00:06 +0300

    ✨ feat(migrations): add `is_min_price` and `is_max_price` to `produce_prices` table
    Extended the `produce_prices` schema with boolean fields `is_min_price` and `is_max_price`. These fields provide additional metadata to distinguish the pricing behavior of produce items.
    
    This enhances the application's capability to handle more detailed pricing logic.

--- database/migrations/2025_10_31_214215_create_produce_prices_table.php
+++ database/migrations/2025_10_31_214215_create_produce_prices_table.php
@@ -16,6 +16,8 @@ return new class extends Migration {
             $table->string('produce_name', 20);
             $table->decimal('min_price', 10, 3)->default(0);
             $table->decimal('max_price', 10, 3)->default(0);
+            $table->boolean('is_min_price')->default(false);
+            $table->boolean('is_max_price')->default(false);
             $table->boolean('is_active')->default(true);
             $table->integer('sort_order')->default(0);
             $table->timestampTz('created_at')->useCurrent();
