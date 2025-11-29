e25620cba50612b2818cdfa4688bf9ad415e67ab
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-11-29 12:50:18 +0300

    ✨ feat(data): allow nullable `emailAddress` and `phoneNumber` in `UserInfoData`
    Updated the `emailAddress` and `phoneNumber` properties in the `UserInfoData` class to be nullable. This change ensures the class can handle scenarios where these fields may not always be present.

--- app/Data/UserInfoData.php
+++ app/Data/UserInfoData.php
@@ -11,12 +11,13 @@ use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 class UserInfoData extends Data
 {
     public string $deviceToken;
+
     public string $farmName;
     public string $firstName;
     public string $lastName;
 
-    public string $emailAddress;
-    public string $phoneNumber;
+    public ?string $emailAddress;
+    public ?string $phoneNumber;
     public string $gender;
     public bool $sendEmail;
     public bool $sendSms;
