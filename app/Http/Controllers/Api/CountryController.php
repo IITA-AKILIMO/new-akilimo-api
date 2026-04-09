b77fa5fc5fab02653a8970e3bc289702447a7219
masgeek <barsamms@gmail.com>
2026-04-09 15:40:51 +0300

    refactor(api): accept request in country index action

--- app/Http/Controllers/Api/CountryController.php
+++ app/Http/Controllers/Api/CountryController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CountryResource;
 use App\Repositories\CountryRepo;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CountryController extends Controller
 {
     public function __construct(protected CountryRepo $repo) {}
 
-    public function index(): AnonymousResourceCollection
+    public function index(Request $request): AnonymousResourceCollection
     {
         $countries = $this->repo->allActive();
 
